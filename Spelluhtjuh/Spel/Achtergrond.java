28946e30953cea5e3063918653fb7d10927cc9a1
deem <116343545+smoothpancake40@users.noreply.github.com>
2022-11-04 09:40:31 +0100

    Create Achtergrond.java

---
+++ Spelluhtjuh/Spel/Achtergrond.java
@@ -0,0 +1,17 @@
+package Spel;
+import java.awt.Image;
+import java.awt.Graphics2D;
+
+public class Achtergrond extends SpelObject
+{
+    public Image plaatje;
+    
+    public Achtergrond(int breedte, int hoogte, Image plaatje){
+        super(0, 0, breedte, hoogte);
+        this.plaatje = plaatje;
+    }
+    
+    public void teken(Graphics2D g){
+        g.drawImage(plaatje, (int)x, (int)y, breedte, hoogte, null); 
+    }
+}
