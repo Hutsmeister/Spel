d0521ba083abcf62bddeae441a3a178b034abd1a
deem <116343545+smoothpancake40@users.noreply.github.com>
2022-11-04 10:12:58 +0100

    Update Stoel.java

--- Spelluhtjuh/Spel/Stoel.java
+++ Spelluhtjuh/Spel/Stoel.java
@@ -1,9 +1,10 @@
 package Spel;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.Image;
 
 public class Stoel extends SpelObject{
-    public Stoel(int x, int y, int breedte, int hoogte, image Plaatje){
+    public Stoel(int x, int y, int breedte, int hoogte, Image plaatje){
         super(x,y,breedte, hoogte);
         this.plaatje = "plaatjes/stoel.png";
     }
