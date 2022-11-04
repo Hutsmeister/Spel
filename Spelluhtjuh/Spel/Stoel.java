0b2193a6d0f59c310dc16fb9f9d65ba3b3ad47d8
deem <116343545+smoothpancake40@users.noreply.github.com>
2022-11-04 10:07:43 +0100

    Update Stoel.java

--- Spelluhtjuh/Spel/Stoel.java
+++ Spelluhtjuh/Spel/Stoel.java
@@ -2,24 +2,13 @@ package Spel;
 import java.awt.Graphics2D;
 import java.awt.Color;
 
-/**
- * class Stoel - geef hier een beschrijving van deze class
- *
- * @author (Demi)
- * @version (versie nummer of datum)
- */
-public class Stoel extends SpelObject
-{
-    private Color kleur;
-    
-    public Obstakel(int x, int y, int breedte, int hoogte, Color kleur){
-        super(x, y, breedte, hoogte);
-        this.kleur = kleur;
+public class Stoel extends SpelObject{
+    public Stoel(int x, int y, int breedte, int hoogte, image Plaatje){
+        super(x,y,breedte, hoogte);
+        this.plaatje = "plaatjes/stoel.png"
     }
-    
     public void teken(Graphics2D g){
-        g.setColor(kleur);
-        g.fillRect((int)x, (int)y, breedte, hoogte);
+        g.fillRect((int)x, (int)y,breedte, hoogte);
     }
-    
 }
+
