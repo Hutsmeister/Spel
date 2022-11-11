0c955eb549b68779167c41da3ed5bcebef2bccad
Hutsmeister <116343505+Hutsmeister@users.noreply.github.com>
2022-11-11 09:55:06 +0100

    Update Tafel.java

--- Spelluhtjuh/Spel/Tafel.java
+++ Spelluhtjuh/Spel/Tafel.java
@@ -2,6 +2,7 @@
 package Spel;
 import java.awt.Graphics2D;
 import java.awt.Color;
+import java.awt.Image;
 
 public class Tafel extends SpelObject{
     public Tafel(int x, int y, int breedte, int hoogte, image Plaatje){
