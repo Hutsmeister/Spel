09727067f098198cc76e9827dac3614bd1640cab
deem <116343545+smoothpancake40@users.noreply.github.com>
2022-11-11 09:54:20 +0100

    Update Tafel.java

--- Spelluhtjuh/Spel/Tafel.java
+++ Spelluhtjuh/Spel/Tafel.java
@@ -6,7 +6,7 @@ import java.awt.Color;
 public class Tafel extends SpelObject{
     public Tafel(int x, int y, int breedte, int hoogte, image Plaatje){
         super(x,y,breedte, hoogte);
-        this.plaatje = "plaatjes/tafel.png"
+        this.plaatje = "plaatjes/tafel.png";
     }
     public void teken(Graphics2D g){
         g.fillRect((int)x, (int)y,breedte, hoogte);
