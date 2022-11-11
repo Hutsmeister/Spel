f560bb9f807820329978ade43109d82e9b5fe968
Hutsmeister <116343505+Hutsmeister@users.noreply.github.com>
2022-11-11 09:55:47 +0100

    Update Sleutel.java

--- Spelluhtjuh/Spel/Sleutel.java
+++ Spelluhtjuh/Spel/Sleutel.java
@@ -3,7 +3,7 @@ package Spel;
 import java.awt.Graphics2D;
 import java.awt.Image;
 
-public class Sleutel extends SpelObject{
+public class Sleutel{
   public Sleutel(int x, int y, boolean isOpgepakt, image plaatje){
     super(x,y,breedte, hoogte);
     this.plaatje = "plaatjes/sleutel.png";
