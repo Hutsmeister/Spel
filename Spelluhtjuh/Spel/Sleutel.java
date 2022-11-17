30b750c3a6073124653e32dfc1dc2a886568df0c
sam <116342927+polekkie@users.noreply.github.com>
2022-11-17 09:01:52 +0100

    Update Sleutel.java

--- Spelluhtjuh/Spel/Sleutel.java
+++ Spelluhtjuh/Spel/Sleutel.java
@@ -6,11 +6,19 @@ import java.awt.Image;
 public class Sleutel extends SpelObject{
   Image plaatje;
   public boolean isOpgepakt;
+  int x;
+  int y;
   
   public Sleutel(int x, int y, Image plaatje){
-    super(x,y,30, 30);
-    isOpgepakt false;
+    super(x,y, breedte, hoogte);
+    breedte = 30; 
+    hoogte = 30;
+    this.x = x;
+    this.y = y;
+    isOpgepakt = false;
     this.plaatje = plaatje;
   }
-
+  public void teken(Graphics2D g){
+    g.drawImage(plaatje, (int)x, (int)y, breedte, hoogte, null);
+  }
 }
