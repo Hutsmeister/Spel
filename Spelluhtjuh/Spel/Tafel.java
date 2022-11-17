f7520590c3c27b5f7de4bc3a9a224f4b92ad0950
Hutsmeister <116343505+Hutsmeister@users.noreply.github.com>
2022-11-17 09:01:16 +0100

    Update Tafel.java

--- Spelluhtjuh/Spel/Tafel.java
+++ Spelluhtjuh/Spel/Tafel.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 import java.awt.Image;
 
 public class Tafel extends SpelObject{
-    public Tafel(int x, int y, int breedte, int hoogte, image Plaatje){
+    public Tafel(int x, int y, int breedte, int hoogte, Image Plaatje){
         super(x,y,breedte, hoogte);
 
     }
