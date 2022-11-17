1308cf1733b7b9efa8cb0af2b5b6fd7842130b2f
Hutsmeister <116343505+Hutsmeister@users.noreply.github.com>
2022-11-17 08:43:17 +0100

    Update Stoel.java

--- Spelluhtjuh/Spel/Stoel.java
+++ Spelluhtjuh/Spel/Stoel.java
@@ -7,7 +7,7 @@ import java.awt.Image;
 public class Stoel extends SpelObject{
     public Stoel(int x, int y, int breedte, int hoogte, Image plaatje){
         super(x,y,breedte, hoogte);
-        this.plaatje = "plaatjes/stoel.png";
+
     }
     public void teken(Graphics2D g){
         g.fillRect((int)x, (int)y,breedte, hoogte);
