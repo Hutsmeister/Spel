fd0c3934a3a1a31ad8ca253bc3e2897221f775db
sam <116342927+polekkie@users.noreply.github.com>
2022-11-17 09:21:13 +0100

    Update Tafel.java

--- Spelluhtjuh/Spel/Tafel.java
+++ Spelluhtjuh/Spel/Tafel.java
@@ -1,15 +1,16 @@
  // author Felix
 package Spel;
 import java.awt.Graphics2D;
-import java.awt.Color;
 import java.awt.Image;
 
-public class Tafel extends SpelObject{
-    public Tafel(int x, int y, int breedte, int hoogte, Image Plaatje){
-        super(x,y,breedte, hoogte);
 
+public class Tafel extends SpelObject{
+    Image plaatje;
+    public Tafel(int x, int y, int breedte, int hoogte, Image Plaatje){
+        super(x,y,breedte, hoogte, null);
+        this.plaatje = plaatje;
     }
     public void teken(Graphics2D g){
-        g.fillRect((int)x, (int)y,breedte, hoogte);
+        g.drawImage(plaatje, (int)x, (int)y, breedte, hoogte;
     }
 }
