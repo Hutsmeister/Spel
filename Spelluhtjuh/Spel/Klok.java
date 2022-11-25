17e5704572a3e2f4fca5d6683ce5ad3eac021a4f
deem <116343545+smoothpancake40@users.noreply.github.com>
2022-11-25 10:20:42 +0100

    Create Klok.java

---
+++ Spelluhtjuh/Spel/Klok.java
@@ -0,0 +1,52 @@
+//author Demi
+package Spel;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+
+public class Klok extends BeweegObject{
+    public String melding;
+    public boolean status, tijdIsOm;
+    public int snelheid, alarmtijd, verlopenTijd;
+    public long vorigeTijd;
+    
+    public Klok(int x, int y, int breedte, int hoogte, int alarmtijd){
+        super(x,y,breedte,hoogte);
+        status = true;
+        snelheid = 1000000000;
+        this.alarmtijd = alarmtijd;
+        tijdIsOm = false;
+        verlopenTijd = 0;
+        vorigeTijd = System.nanoTime();
+        melding = "Gestopt";
+    }
+    
+    public void beweeg(float s){
+        loopt();
+    }
+    
+    public void loopt(){
+        long now = System.nanoTime();
+        if(status){
+            if(now-vorigeTijd >= snelheid){
+                verlopenTijd ++;
+                melding = "Time "+ verlopenTijd;
+                vorigeTijd = now;
+            }
+            if(verlopenTijd >= alarmtijd){
+                status = false;
+                tijdIsOm = true;
+                melding = "Time "+ verlopenTijd;
+            }
+        }
+    }
+    
+    public void teken(Graphics2D g){
+        g.setColor(Color.CYAN);
+        g.fillRect((int)x, (int)y, breedte, hoogte);
+        g.setFont(new Font(Font.SERIF, Font.ITALIC, 18));
+        g.setColor(Color.RED);
+        g.drawString(melding, x+5, y+30);
+    }
+}
