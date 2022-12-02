fc0c515b8821a2c0e5b6fb667196cf5835e056b2
sam <116342927+polekkie@users.noreply.github.com>
2022-12-02 10:31:24 +0100

    Create Hart.java

---
+++ Spelluhtjuh/Spel/Hart.java
@@ -0,0 +1,15 @@
+package Spel;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+
+public class Hart extends SpelObject{
+    Image plaatje;
+    public Hart(int x, int y, int breedte, int hoogte, Image plaatje){
+        super(x,y,breedte, hoogte);
+        this.plaatje = plaatje;
+    }
+    public void teken(Graphics2D g){
+        g.drawImage(plaatje, (int)x, (int)y, breedte, hoogte, null);
+    }
+}
