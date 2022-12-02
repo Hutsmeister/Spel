31735ad98a040a29136e1c146127c50cd96c49e1
deem <116343545+smoothpancake40@users.noreply.github.com>
2022-12-02 09:51:17 +0100

    Update Omgeving.java

--- Spelluhtjuh/Spel/Omgeving.java
+++ Spelluhtjuh/Spel/Omgeving.java
@@ -57,7 +57,7 @@ public class Omgeving {
         muis = m;
         // geef hier de opdracthen om de methoden uit te voeren
         // die de objecten maken.
-        klok = new Klok(862, 102, 105, 68, 900);
+        klok = new Klok(910, 64, 100, 50, 900);
         
         maakAchtergrond();
         maakSpecialeBeweegObjecten();
